9fb0ff8a48a32294a9657d9c3b16b27f42dc8702
dannymolyneux <dmolyneux@colgate.edu>
2025-04-22 14:36:40 -0400

    1b complete

--- Lab13Script.R
+++ Lab13Script.R
@@ -3,6 +3,7 @@
 #part a
 library(tidyverse)
 library(e1071)
+library(ggplot2)
 data = read_csv("zebrafinches.csv")
 far.vec = data$further
 mu0 = 0
@@ -10,3 +11,11 @@ n = 25
 far.t.stat = (mean(far.vec) - mu0)/(sd(far.vec)/sqrt(n)) #far t statistic
 far.pdf = dnorm(far.t.stat)
 far.error = (skewness(far.vec)/sqrt(n))*((2*far.t.stat^2 + 1)/6)*far.pdf
+
+t.stat = seq(-10,10, by = 0.01)
+error.vec = (skewness(far.vec)/sqrt(n))*((2*t.stat^2 + 1)/6)*dnorm(t.stat)
+error.dat = data.frame(t = t.stat, error = error.vec)
+error.plot = ggplot(data = error.dat, aes(x = t, y = error))+
+  geom_line(color = "red") +
+  labs(title = "Error with respect to t statistic for further data")
+error.plot
